0a20e85d91f17efe2d6e6d7d32aa0f117d94043a
shrutitaylor <shruti128770@gmail.com>
2024-04-18 14:32:51 +1000

    modified css for mobile

--- src/componets/main/ProjectsIntro.js
+++ src/componets/main/ProjectsIntro.js
@@ -2,12 +2,12 @@ import React from "react";
 const ProjectsIntro = () => {
   return (
     <>
-    <section className="main about_me"  style={{margin: '150px'}}>
-    <div className="about_title">
+    <section className="main about_me"  >
+    <div className="project_intro">
       <h3 style={{fontSize: '25px'}}>PROJECTS</h3>
-      <div className=" about_title">
-          <h1 style={{fontSize: '50px', color: 'white'}}>Curious what I've done?</h1>
-      </div>
+      
+          <h1 style={{fontSize: '50px', color: 'white', alignSelf:'center', textAlign:'center'}}>Curious what I've done?</h1>
+      
       </div>
       </section>
        
